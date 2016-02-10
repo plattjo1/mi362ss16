25903c0f9da1d5bd19f5123c471c3ba53b61a5b6
plattjo1 <plattjo1@msu.edu>
2016-02-09 22:10:33 -0800

    Week 4 Hello, Node update
    adds week 4 and the node  server.js update

---
+++ Week 4/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.createServer( (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h1>Hello World</h1>\n');
+  response.write('<p1>This is a node app</p>\n');
+  response.end();
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
